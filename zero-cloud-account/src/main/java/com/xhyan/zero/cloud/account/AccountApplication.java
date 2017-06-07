68d346f2627f28935d5f6cc9680855828bdf0828
yanliwei <yanliwei@gome.com.cn>
2017-06-07 20:37:00 +0800

    spring boot admin 配置修改

--- zero-cloud-account/src/main/java/com/xhyan/zero/cloud/account/AccountApplication.java
+++ zero-cloud-account/src/main/java/com/xhyan/zero/cloud/account/AccountApplication.java
@@ -3,6 +3,8 @@ package com.xhyan.zero.cloud.account;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -11,6 +13,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @author yanliwei
  */
 @EnableSwagger2
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan(basePackages = "com.xhyan.zero.cloud.account.mapper")
 public class AccountApplication {
--- zero-cloud-member/src/main/java/com/xhyan/zero/cloud/member/MemberApplication.java
+++ zero-cloud-member/src/main/java/com/xhyan/zero/cloud/member/MemberApplication.java
@@ -1,14 +1,13 @@
 package com.xhyan.zero.cloud.member;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
  * spring cloud eureka server 启动程序
  */
-@EnableAdminServer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MemberApplication {
