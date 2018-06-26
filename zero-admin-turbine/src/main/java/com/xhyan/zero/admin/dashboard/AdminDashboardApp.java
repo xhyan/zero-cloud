0fe73fa5fecf58a689891b43d27bdc1d07b71e0b
xhyan <imissyou.xhyan@gmail.com>
2018-06-26 17:46:41 +0800

    修改admin,简化配置

--- zero-admin-turbine/src/main/java/com/xhyan/zero/admin/dashboard/AdminDashboardApp.java
+++ zero-admin-turbine/src/main/java/com/xhyan/zero/admin/dashboard/AdminDashboardApp.java
@@ -1,14 +1,10 @@
 package com.xhyan.zero.admin.dashboard;
 
-import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 
 @Configuration
@@ -19,45 +15,6 @@ public class AdminDashboardApp {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(AdminDashboardApp.class).run(args);
-    }
-
-//    @Profile("insecure")
-//    @Configuration
-//    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            http.authorizeRequests().anyRequest().permitAll()//
-//                .and().csrf().disable();
-//        }
-//    }
-
-    @Configuration
-    public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
-
-        private final String adminContextPath;
-
-        public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
-            this.adminContextPath = adminServerProperties.getContextPath();
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            // @formatter:off
-            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-            successHandler.setTargetUrlParameter("redirectTo");
-
-            http.authorizeRequests()
-                .antMatchers(adminContextPath + "/assets/**").permitAll()
-                .antMatchers(adminContextPath + "/login").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler)
-                .and()
-                .logout().logoutUrl(adminContextPath + "/logout").and()
-                .httpBasic().and()
-                .csrf().disable();
-            // @formatter:on
-        }
     }
 
 }
