21f35001f36515bb05c2edd233f319f30d4ff55e
xhyan <imissyou.xhyan@gmail.com>
2018-06-27 16:09:42 +0800

    gateway 模块有netflix zuul升级成spring cloud gateway

--- zero-cloud-gateway/src/main/java/com/xhyan/zero/gateway/GatewayApplication.java
+++ zero-cloud-gateway/src/main/java/com/xhyan/zero/gateway/GatewayApplication.java
@@ -1,5 +1,6 @@
 package com.xhyan.zero.gateway;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 
@@ -8,7 +9,7 @@ import org.springframework.cloud.client.SpringCloudApplication;
  *
  * @author xhyan
  */
-@SpringCloudApplication
+@SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
